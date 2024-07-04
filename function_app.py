5e023f1e97ff6dc14ab580e223e2cba0d4b516b9
yashizzzhang <161668644+yashizzzhang@users.noreply.github.com>
2024-07-04 13:48:16 +0800

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import modules.task
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 5 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True) 
+@app.schedule(schedule="0 0 6 * * *", arg_name="myTimer", run_on_startup=False, use_monitor=True) 
 def func_netsuite_frs(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
@@ -12,3 +12,4 @@ def func_netsuite_frs(myTimer: func.TimerRequest) -> None:
 
     modules.task.main()
     logging.info('func_netsuite_frs function executed.')
+ 
